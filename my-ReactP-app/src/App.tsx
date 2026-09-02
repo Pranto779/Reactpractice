14fcb978168ef1d98f3048c3b011974f57fd6ca6
Pranto779 <pranto77268@gmail.com>
2026-09-02 18:06:49 +0600

    React-Practice

---
+++ my-ReactP-app/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Getprops from './Props'
+
+function App() {
+
+
+  return (
+    <>
+      <Getprops name="pranto" age="22"></Getprops>
+    </>
+  )
+}
+
+export default App
---
+++ my-ReactP-app/src/Props.tsx
@@ -0,0 +1,12 @@
+function Getprops(props){
+
+    return(
+
+        <div style={{color:"white", padding:"5px 10px ", border:"1px solid pink", borderRadius:"10px"}}>
+            <p>Name:{props.name} </p>
+            <p>Age:{props.age} </p>
+        </div>
+    )
+}
+
+export default Getprops
